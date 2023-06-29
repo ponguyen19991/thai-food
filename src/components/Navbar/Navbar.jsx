7b91d7e327eb40f909671ef4501695cbe529740d
nguyen-bui201299 <nguyen.bui201299@stechvn.org>
2023-06-30 00:29:46 +0700

    fix-Some-responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,17 +17,17 @@ const Navbar = () => {
                     <FaTimes/>
                 </button>
                 <ul>
-                    <li><NavLink to="/" activeClassName="active" className="home-page">Home Page</NavLink></li>
-                    <li><NavLink to="/restaurants" activeClassName="active" className="restaurants">Restaurants</NavLink></li>
-                    <li><NavLink to="/today" activeClassName="active" className="offers">Today's Offers</NavLink></li>
-                    <li><Link to="/" className="site-title"><img src="/images/ThaiLogo.png" alt="Logo" width={120} height={120} /></Link></li>
-                    <li><NavLink to="/contact-us" activeClassName="active" className="contact-us">Contact Us</NavLink></li>
-                    <li><NavLink to="/about-us" activeClassName="active" className="about-us">About Us</NavLink></li>
-                    <li><NavLink to="/how-we-work" activeClassName="active" className="how-we-work">How we Work?</NavLink></li>
+                    <li><NavLink to="/" activeClassName="active" className="home-page" onClick={showNavbar}>Home Page</NavLink></li>
+                    <li><NavLink to="/restaurants" activeClassName="active" className="restaurants" onClick={showNavbar}>Restaurants</NavLink></li>
+                    <li><NavLink to="/today" activeClassName="active" className="offers" onClick={showNavbar}>Today's Offers</NavLink></li>
+                    <li><Link to="/" className="site-title" onClick={showNavbar}><img src="/images/ThaiLogo.png" alt="Logo" width={120} height={120} /></Link></li>
+                    <li><NavLink to="/contact-us" activeClassName="active" className="contact-us" onClick={showNavbar}>Contact Us</NavLink></li>
+                    <li><NavLink to="/about-us" activeClassName="active" className="about-us" onClick={showNavbar}>About Us</NavLink></li>
+                    <li><NavLink to="/how-we-work" activeClassName="active" className="how-we-work" onClick={showNavbar}>How we Work?</NavLink></li>
                 </ul>
             </nav>
             <div className='responsive-site'>
-                <NavLink to="/" className="site-title-responsive">Site Name</NavLink>
+                <NavLink to="/" className="site-title-responsive"><img src="/images/ThaiLogo.png" alt="Logo" width={120} height={120} /></NavLink>
                 <button className='nav-btn'>
                     <FaBars onClick={showNavbar}/>
                 </button>
