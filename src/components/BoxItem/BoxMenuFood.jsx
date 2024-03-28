14621a8baebd22985dba503540b0668a268443a1
nguyen-bui201299 <nguyen.bui201299@stechvn.org>
2024-03-28 14:05:38 +0700

    update style

--- src/components/BoxItem/BoxMenuFood.jsx
+++ src/components/BoxItem/BoxMenuFood.jsx
@@ -14,43 +14,42 @@ const BoxMenuFood = ({ dish, openModal, t, handleAddToCart, handleClickAlert }) 
   return (
     <Box>
       <Box
-      sx={{
-        bgcolor: '#fff',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: '1rem',
-        boxShadow: 'rgba(255, 255, 255, 0.15) 1.95px 1.95px 2.6px',
-        borderRadius: '2px',
-        userSelect: 'none',
-        cursor: 'pointer',
-        transition: 'transform 0.3s ease',
-        '&:hover': {
-          transform: 'scale(1.02)',
-        },
-      }}
-      onClick={() => openModal(dish)}
-    >
-      <img src={dish.src} alt={dish.name} style={{ width: '100%', height: 'auto' }} />
-      <Typography variant="h5" component="h2" sx={{ mt: 1, fontWeight: '550' }}>
-        {t(dish.name)}
-      </Typography>
-      <Typography variant="body1" sx={{ mt: 1, fontStyle: 'italic', height: '80px', display: 'flex', alignItems: 'center' }}>
-        {t(dish.description)}
-      </Typography>
-      <Typography variant="body2" sx={{ mt: 1, color: '#cf3835', fontWeight: '550' }}>
-        ${t(dish.price)}
-      </Typography>
-      <Button onClick={handleClick}>
-          <FaShoppingCart />
-      </Button>
-        
+        sx={{
+          bgcolor: '#fff',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '1rem',
+          boxShadow: 'rgba(255, 255, 255, 0.15) 1.95px 1.95px 2.6px',
+          borderRadius: '2px',
+          userSelect: 'none',
+          cursor: 'pointer',
+          transition: 'transform 0.3s ease',
+          '&:hover': {
+            transform: 'scale(1.02)',
+          },
+        }}
+        onClick={() => openModal(dish)}
+      >
+        <img src={dish.src} alt={dish.name} style={{ width: '100%', height: 'auto' }} />
+        <Typography variant="h5" component="h2" sx={{ mt: 1, fontWeight: '550' }}>
+          {t(dish.name)}
+        </Typography>
+        <Typography variant="body1" sx={{ mt: 1, fontStyle: 'italic', height: '80px', display: 'flex', alignItems: 'center' }}>
+          {t(dish.description)}
+        </Typography>
+        <Typography variant="body2" sx={{ mt: 1, color: '#cf3835', fontWeight: '550' }}>
+          ${t(dish.price)}
+        </Typography>
+        <Button onClick={handleClick} sx={{ backgroundColor: '#E45646', color: '#fff', '&:hover': { backgroundColor: 'rgba(228, 86, 70, 0.7)' } }}>
+            <FaShoppingCart fontSize={20}/>
+            <Typography variant="body2" sx={{ fontWeight: '550' }} ml={1}>Add To Cart</Typography>
+        </Button>
+          
+      </Box>
     </Box>
-  
-    </Box>
-    
   );
 };
 
